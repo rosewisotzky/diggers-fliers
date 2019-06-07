cf661e847b0967e0a9adda46304d8a0794319ad8
Rose Wisotzky <rose@rosewisotzky.com>
2019-06-07 11:39:09 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace diggers_fliers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
